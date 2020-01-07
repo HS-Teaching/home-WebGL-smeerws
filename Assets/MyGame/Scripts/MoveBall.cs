19d77c51f0c01e5f693659f31ebd2d6d90628b61
smeerws <hello@smeerws.at>
2020-01-07 23:29:10 +0100

    add gui to mainscene (ball, positions as empty GO, Button, Text) add script move ball to move the ball when pressing the button: -- Instanzvariables: ball,  GO Array with 4 positions -- 3 methods: ChangeBallPosition (onclick from button) using GetDifferentRandomNumber(), GetRandomNumber (returns a random number between 0-4) repetition is possible GetDifferentRandomNumber (returns a random number between 0-4) without repetition, new Random number != last Random number switch player to WebGL add docs folder for WebGL Build Build WebGL into docs

---
+++ Assets/MyGame/Scripts/MoveBall.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] ballPositions;
+
+    private Vector3 posBallDefault;
+
+    [SerializeField]
+    private GameObject ball;
+
+    int lastRandomIndex;
+
+    private void Start()
+    {
+        posBallDefault = ball.transform.position;
+    }
+
+    public void ChangeBallPosition()
+    {
+        int index = GetDifferentRandomNumber();
+        ball.transform.position = ballPositions[index].transform.position;
+    }
+
+    private int GetRandomNumber()
+    {
+        int index = Random.Range(0, 4);
+        return index;
+    }
+
+    private int GetDifferentRandomNumber()
+    {
+        int newIndex;
+
+        do
+        {
+            newIndex = Random.Range(0, 4);
+        }
+        while (newIndex == lastRandomIndex);
+
+        lastRandomIndex = newIndex;
+
+        return newIndex;
+    }
+}
